4764ebffecf86976ea331ed61ab80b355cd56ab0
Valeriya2511 <finsternis.ru@mail.ru>
2023-11-15 22:19:20 +0600

    feat: solution from 12 to 15 task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -202,8 +202,15 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  const y = [];
+  for (let i = 2; i <= n; i += 1) {
+    if (n % i === 0) {
+      y.push(i);
+    }
+  }
+  if (y.length < 2) return true;
+  return false;
 }
 
 /**
@@ -221,8 +228,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return Number(value) ? Number(value) : def;
 }
 
 /**
@@ -236,8 +243,8 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num ** 3;
 }
 
 /**
@@ -253,8 +260,11 @@ function getCube(/* num */) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index <= 1) {
+    return index;
+  }
+  return getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
 }
 
 /**
